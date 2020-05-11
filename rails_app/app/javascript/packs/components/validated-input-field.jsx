a701db6b4ba054e62e5e93ba84743fddf3db7079
Andre LaFleur <andre.lafleur@utah.edu>
2020-05-10 18:25:03 -0600

    Handle onChange with validated text field

--- rails_app/app/javascript/packs/components/validated-input-field.jsx
+++ rails_app/app/javascript/packs/components/validated-input-field.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function ValidatedInputField(props) {
-    return (
-        <div>
-            <label htmlFor={props.id}>{props.label}</label>
-            <input id={props.id} type="text" />
-        </div>
-    );
+  const onChange = (event) => {
+    const { value } = event.target;
+    props.isValid(value);
+  }
+
+  return (
+    <div>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        id={props.id}
+        onChange={onChange}
+        type="text"
+      />
+    </div>
+  );
 }
 
 ValidatedInputField.propTypes = {
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  isValid: PropTypes.func,
+};
+
+ValidatedInputField.defaultProps = {
+  isValid: () => {},
 };
--- rails_app/app/javascript/packs/components/validated-input-field.test.jsx
+++ rails_app/app/javascript/packs/components/validated-input-field.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, cleanup } from "@testing-library/react";
+import td from 'testdouble';
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import ValidatedInputField from "./validated-input-field";
 
 describe('app/javascript/packs/components/validated-input-field', () => {
@@ -18,6 +19,16 @@ describe('app/javascript/packs/components/validated-input-field', () => {
      it('takes an ID', () => {
        const { getByLabelText } = renderComponent({ id: 'yo' });
        expect(getByLabelText('Some label').getAttribute('id')).toBe('yo');
+     });
+   });
+
+   describe('valdiation API', () => {
+     it('validates its input with a provided function', () => {
+       const isValid = td.func();
+       const { getByLabelText } = renderComponent({ isValid });
+       fireEvent.change(getByLabelText('Some label'), { target: { value: 'foo' } });
+
+       td.verify(isValid('foo'));
      });
    });
 });
