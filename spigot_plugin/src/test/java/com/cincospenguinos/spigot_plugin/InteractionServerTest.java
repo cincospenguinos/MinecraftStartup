027a7c36be0706a98e2cfaff6a75a1fc0d2ef42d
Andre LaFleur <andre.lafleur@utah.edu>
2020-05-25 19:17:20 -0600

    Refactor a bit

--- spigot_plugin/src/test/java/com/cincospenguinos/spigot_plugin/InteractionServerTest.java
+++ spigot_plugin/src/test/java/com/cincospenguinos/spigot_plugin/InteractionServerTest.java
@@ -11,11 +11,13 @@ import static org.junit.Assert.*;
 
 public class InteractionServerTest {
     private InteractionServer server;
+    private static final int TEST_PORT = 25567;
 
     @Before
     public void setup() {
-        server = new InteractionServer(null, 25566);
+        server = new InteractionServer(null, TEST_PORT);
         server.start();
+        rest();
     }
 
     @After
@@ -26,9 +28,16 @@ public class InteractionServerTest {
     @Test
     public void serverAcceptsConnection() {
         try {
+            new Socket("localhost", TEST_PORT);
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    private void rest() {
+        try {
             Thread.sleep(100L);
-            new Socket("localhost", 25566);
-        } catch (IOException | InterruptedException e) {
+        } catch (InterruptedException e) {
             fail();
         }
     }
