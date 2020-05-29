f2e2046e388693d58a3130dc5228350e2e8f9337
BuildTools <unconfigured@null.spigotmc.org>
2020-05-29 22:44:01 +0000

    I guess I needed nothing at all

--- rails_app/config/webpack/production.js
+++ rails_app/config/webpack/production.js
@@ -1,5 +1,4 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-environment.loaders.file.exclude(['test\.jsx', 'test\.js'])
 module.exports = environment.toWebpackConfig()
