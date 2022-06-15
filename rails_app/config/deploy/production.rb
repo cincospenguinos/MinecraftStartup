1e7b103740ac790e224d045123b295a16dfafbb7
Andre LaFleur <cincospenguinos@gmail.com>
2022-06-15 10:34:45 -0600

    Wade through dependency hell

--- rails_app/config/deploy/production.rb
+++ rails_app/config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server '192.168.1.32', user: 'minecraft', roles: %{app db web}
+server ENV['ANDRE_MINECRAFT_SERVER_LOCAL'], user: 'minecraft', roles: %{app db web}
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
