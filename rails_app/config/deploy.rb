382d534194ef9cf7a3f669ea8f7fac0b211f340a
Andre LaFleur <cincospenguinos@gmail.com>
2022-06-22 18:58:50 -0600

    Still figuring out deploying

--- rails_app/config/deploy.rb
+++ rails_app/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.0"
+lock "~> 3.17.0"
 
 set :application, "rails_app"
 set :repo_url, "git@github.com:cincospenguinos/MinecraftStartup.git"
