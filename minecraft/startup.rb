d6be4c668e830e5575318c245cf5828c70625e34
Andre LaFleur <cincospenguinos@gmail.com>
2022-07-08 16:19:08 -0600

    Add another startup script to handle fresh server without mounted volume

---
+++ minecraft/startup.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+require 'spigot_interface'
+
+puts 'Checking for minecraft files...'
+has_minecraft = Dir['/root/minecraft/**.jar'].any? { |f| f.include?('minecraft-server.jar') }
+unless has_minecraft
+	puts 'Minecraft server not currently installed. Installing...'
+	FileUtils.mv('/tmp/build_spigot/minecraft-server.jar', '/root/minecraft')
+	puts `java -jar minecraft-server.jar`
+	FileUtils.rm('eula.txt')
+	File.open('eula.txt', 'w') { |f| f.puts "eula=true" }
+	FileUtils.mkdir_p('plugins')
+	FileUtils.mv('/tmp/spigot_plugin/AndreServerPlugin.jar', 'plugins/')
+	puts 'Done.'
+end
+
+puts 'Starting spigot interface server...'
+`spigot_interface serve #{ENV['MINECRAFT_STATUS_PORT']}`
