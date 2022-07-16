cbb553e18a9ab27208c5be82165fb18ed9e32718
Andre LaFleur <cincospenguinos@gmail.com>
2022-07-16 16:48:33 -0600

    Remove unneeded deploy vars

--- rails_app/config/deploy.rb
+++
@@ -1,9 +0,0 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.0"
-
-set :application, "rails_app"
-set :repo_url, "git@github.com:cincospenguinos/MinecraftStartup.git"
-set :deploy_to, '/home/minecraft/rails_app'
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-set :keep_releases, 3
-set :bundle_gemfile, 'rails_app/Gemfile'
