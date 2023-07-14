5a445a690f667fb78d37f5b016526439e201a4dc
Andre LaFleur <cincospenguinos@gmail.com>
2023-07-14 14:03:46 -0600

    WIP

--- rails_app/app/controllers/startup_controller.rb
+++ rails_app/app/controllers/startup_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class StartupController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @csrf_token = form_authenticity_token
   end
